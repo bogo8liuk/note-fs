c64c1bdcc12eadfa6d281e6842c863a6497430e9
bogo8liuk <lucaborghi99@gmail.com>
2023-05-27 17:10:51 +0200

    using Haskeline library for the repl

--- src/Repl.hs
+++ src/Repl.hs
@@ -3,8 +3,9 @@ module Repl
 ) where
 
 import Control.Monad (unless)
-import System.IO
-import Config
+import Control.Monad.Trans.Class (lift)
+import System.Console.Haskeline hiding (display)
+import Config hiding (historyFile)
 import Env
 import Commands
 import Commands.Impl
@@ -14,12 +15,40 @@ import Commands.Parsing as Parsing
 emptyProg :: String
 emptyProg = ""
 
-fetchOpts :: String -> NotesKeeper (String, [String])
+fetchOpts :: String -> (String, [String])
 fetchOpts input =
+    {- TODO: using `words` is wrong, because arguments can be enclosed between "" -}
     case words input of
-        [] -> return (emptyProg, [])
-        (w : ws) -> return (w, ws)
+        [] -> (emptyProg, [])
+        (w : ws) -> (w, ws)
 
+replInput :: InputT NotesKeeper ()
+replInput = do
+    input <- getInputLine Config.replPrompt
+    case input of
+        {- `Nothing` has the semantics of "end-of-file", which is, then, implemented as the exit command -}
+        Nothing -> runCommand' EXIT
+        Just cmdStr -> do
+            let (prog, args) = fetchOpts cmdStr
+            isEnd <- continueOnError $ manageCommand prog args
+            unless isEnd replInput
+    where
+        continueOnError op =
+            lift (op `catchError` handleErrorWith (return False))
+
+        manageCommand prog args
+            | prog == Lexer.replEmptyProg =
+                return True
+            | otherwise = do
+                cmd <- Parsing.anyCommand prog args
+                runCommand cmd
+                {- Special handling for exit command, since it actually cannot have the control of the repl: it's not
+                up to the exit command op-code to know the repl logic. -}
+                return $ isExitCmd cmd
+
+        runCommand' = lift . runCommand
+
+{-
 purePerform :: NotesKeeper ()
 purePerform = retryOnError $ do
     displayPrompt
@@ -41,10 +70,16 @@ purePerform = retryOnError $ do
             display Config.replPrompt
             {- Flushing the prompt, since it has no newline character. -}
             performIO $ hFlush stdout
+-}
+
+settings :: FilePath -> Settings NotesKeeper
+settings history =
+    defaultSettings { historyFile = Just history }
 
 perform :: NotesKeeper ()
 perform = do
-    purePerform
+    history <- getHistoryPath
+    runInputT (settings history) replInput
     {- The changes are committed only at the end of the repl. This offers a better efficiency, since IO actions on the
     file-system could be expensive. -}
     commitChanges
